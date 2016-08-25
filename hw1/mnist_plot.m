2e249fce88344426f12e4fce7f92dfb173803e71
agjayant <agjayant@rediff.com>
2016-08-26 00:24:37 +0530

    fix : /T

--- hw1/mnist_plot.m
+++ hw1/mnist_plot.m
@@ -1,16 +1,16 @@
 load mnist_hw1.mat;
-
-accuracy_vector = zeros([1 40]);
-for i = 1:40
+num = 40;
+accuracy_vector = zeros([1 num]);
+for i = 1:num
     accuracy_vector(i) = predict_digit(50*i,dataX,X_test,Y_test);
 end
 
 accuracy_vector = accuracy_vector*100;
 
-X = 1:40;
+X = 1:num;
 X = X*50;
 
-accuracy_all = ones([1 40]);
+accuracy_all = ones([1 num]);
 accuracy_all = accuracy_all*mnist_classifier(dataX,X_test,Y_test)*100;
 
 figure1 = figure;
@@ -18,5 +18,5 @@ plot(X,accuracy_vector,'--*',X,accuracy_all);
 title('MNIST CLASSIFIER');
 xlabel('Number of Training Examples')
 ylabel('Accuracy(%)')
-legend('Random Samples from Training Examples','Max Accuracy : Using all Training Examples');
+legend('Using Random Samples from Training Examples','Using all Training Examples');
 saveas(figure1,'plot_mnist.jpg');
