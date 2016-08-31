f6c7ce9b83ed4e8401d5c954965b1ddc45aac685
agjayant <agjayant@rediff.com>
2016-08-31 23:53:24 +0530

    hw1-final

---
+++ hw1/hw1/Readme.md
@@ -0,0 +1,9 @@
+# Code for Problem 6
+mnist_plot.m
+(Uses functions defined in files mnist_classifier.m and predict_digit.m)
+
+#Plot for Problem 6
+mnist_plot.jpg
+
+#Writeup (Problems 1-6)
+report,pdf
---
+++ hw1/hw1/mnist_plot.m
@@ -0,0 +1,22 @@
+load mnist_hw1.mat;
+num = 40;
+accuracy_vector = zeros([1 num]);
+for i = 1:num
+    accuracy_vector(i) = predict_digit(50*i,dataX,X_test,Y_test);
+end
+
+accuracy_vector = accuracy_vector*100;
+
+X = 1:num;
+X = X*50;
+
+accuracy_all = ones([1 num]);
+accuracy_all = accuracy_all*mnist_classifier(dataX,X_test,Y_test)*100;
+
+figure1 = figure;
+plot(X,accuracy_vector,'--*',X,accuracy_all);
+title('MNIST CLASSIFIER');
+xlabel('Number of Training Examples')
+ylabel('Accuracy(%)')
+legend('Using Random Samples from Training Examples','Using all Training Examples');
+saveas(figure1,'mnist_plot.jpg');
